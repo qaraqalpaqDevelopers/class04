bbef9491cb19c41e4975cb434d5d751be757fb68
Bakhtjan <108337927+Bakhtjan@users.noreply.github.com>
2022-08-16 21:09:32 +0500

    Добавьте файлы проекта.

---
+++ asd/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd
+{
+    internal class Student
+    {
+        public string ati;
+        public int jili;
+        public string tamamlagan;
+        public string kayjerge;
+        public int kirgenjili; 
+        public string fakultet;    
+
+
+
+        public void maglumat(string name, int date, string tamam, /*int tamam1*/string kirgenoxiw,int jilikirgen,string faq)
+        {
+
+            Console.WriteLine($"{ati} {jili} tuwilgan,{tamam}-sanli mektepti tamamlagan, {kayjerge} {jilikirgen} {faq} fakultetine kirgen" );
+        }
+    }
+}
---
+++ asd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd
+{
+    internal class Class
+    {
+        static void Main(string[] args)
+        {
+            Student kaijerge = new Student();    
+            kaijerge.ati = "Abat Erjanov";
+            kaijerge.jili = 2003;
+            kaijerge.tamamlagan = "21";
+            kaijerge.kayjerge = "Qaraqalpaq mamleketlik universiteti";
+            kaijerge.kirgenjili = 2022;
+            kaijerge.fakultet = "Matematika";
+
+            kaijerge.maglumat(kaijerge.ati,kaijerge.jili,kaijerge.tamamlagan,kaijerge.kayjerge,kaijerge.kirgenjili,kaijerge.fakultet );
+           
+            Console.ReadKey();
+        }         
+    }
+}
